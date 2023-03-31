debd4be339ffd64e92ce50a05ab985493e1adae1
KalpanaVasudevan <64738791+KalpanaVasudevan@users.noreply.github.com>
2023-03-31 22:29:51 +0530

    Add files via upload
    Week1_Day2_Classroom&Assignments

---
+++ day2/CheckIfNumberIsPositiveOrNegative.java
@@ -0,0 +1,23 @@
+package week1.day2;
+
+public class CheckIfNumberIsPositiveOrNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num=35;
+		if (num>0) {
+			System.out.println("Number is Positive Number");
+			
+		}
+		else if(num==0)
+		{
+			System.out.println("Number is neither positive nor negative");
+		}
+		else
+		{
+			System.out.println("Number is negative");
+		}
+	}
+
+}
---
+++ day2/LearnDatatypes.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class LearnDatatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age=30;
+		String name="kalpana";
+		boolean learningSelenium=true;
+		
+		//System.out.println(age+" "+name+" "+learningSelenium);
+		
+		System.out.println(age+"\n"+name+"\n"+learningSelenium);
+	}
+
+}
